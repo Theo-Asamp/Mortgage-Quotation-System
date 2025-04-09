80cd4def32880084c58d37ed9ee20b3fb0c001f9
Raheel654 <c4003682@hallam.shu.ac.uk>
2025-04-09 02:18:20 +0100

    Update privacy.php
    added class="intro-section__text" to add a bit of spacing between the lines and neaten it up

--- privacy.php
+++ privacy.php
@@ -25,15 +25,15 @@ require 'headerFooter.php';
         <div class="intro-section__content">
             <h2 class="intro-section__title"> PRIVACY NOTICE</h2>
             <DIV id = "Role_Privacy"></DIV>
-            <h1>Our Role in your Privacy</h1>
-            <p>At Rose Brokers, your privacy is our priority. We are committed to protecting your personal information and 
+            <h1>Our Role In Your Privacy</h1>
+            <p class="intro-section__text">At Rose Brokers, your privacy is our priority. We are committed to protecting your personal information and 
               ensuring it is handled with the utmost care, transparency, and security. Our role is to collect only what we need, 
               use it responsibly, and never share it without your consent—so you can trust us with your data every step of the way.</p>
             
             <div class = "dropdown">
-              <h1>How we collect and use your Information</h1></div>
+              <h1>How We Collect and Use Your Information</h1></div>
             <div class = "Intro_Section">
-              <p class = "Intro_Class">Under data protection law, an organisation
+              <p class="intro-section__text">Under data protection law, an organisation
                that decides how the personal information it collects
                and holds is used is called a ‘data controller’.
                We call this use of personal information ‘processing’.
@@ -41,15 +41,15 @@ require 'headerFooter.php';
             </p></div>
             <div class id ="your_responsbility" >
               <h1>Your Responsibility</h1>
-              <p>At Rose Brokers, we believe privacy is a shared responsibility.
+              <p class="intro-section__text">At Rose Brokers, we believe privacy is a shared responsibility.
                  We’re committed to safeguarding your data, but we also encourage you to stay informed
                   about how your information is used. Have you reviewed your privacy settings and understood your rights? 
                  Knowing your responsibilities helps us protect what matters most—your trust.</p>
             </div>
             <div class = "dropdown1">
-              <h1>The information we collect and hold</h1></div>
+              <h1>The Information We Collect and Hold</h1></div>
             <div class = "Dropdown_Section_Information">
-              <p class = "Information_Class">The information we might collect and hold about you includes:
+              <p class="intro-section__text">The information we might collect and hold about you includes:
 <br>Personal information to identify and contact you, such as your name, address, contact details and 
 date of birth. We also rely on biometric information, which may be held by Rose Brokers and our trusted 
 third parties, like your fingerprint or a biometric map of your face and the selfie you provide.
