b10ff828d6b27104eb7ba860e7ffa14972992b98
Raheel654 <c4003682@hallam.shu.ac.uk>
2025-04-09 02:52:45 +0100

    add content to contact page

--- contact.php
+++ contact.php
@@ -22,8 +22,24 @@ require 'headerFooter.php';
 
     <section class="intro-section">
         <div class="intro-section__content">
-            <h2 class="intro-section__title">contact Rose brokers</h2>
-            <p class="intro-section__text">contact us - </p>
+            <h2 class="intro-section__title">Contact Rose Brokers</h2>
+            <p class="intro-section__text">If you have any questions or need support, feel free to get in touch with us using the details below:</p>
+
+            <h1>Phone Number</h1>
+            <p class="intro-section__text">0800 220 99 98</p>
+
+            <h1>Email</h1>
+            <p class="intro-section__text">support@rosebrokers.com</p>
+
+            <h1>Address</h1>
+            <p class="intro-section__text">123 Finance Street<br>
+            Sheffield, S1 4QL<br>
+            United Kingdom</p>
+
+            <h1>Office Hours</h1>
+            <p class="intro-section__text">Monday–Friday: 9:00 AM – 5:00 PM</p>
+
+            <p class="intro-section__text">We look forward to hearing from you!</p>
         </div>
     </section>
 
